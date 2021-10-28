1a9a5c8786696c52bef234fe241d72731ed87d60
Jens Guballa <jens@guballa.de>
2021-10-28 19:58:10 +0200

    Move certificate cache from connection module to cert_chain module

--- tlsmate/tlsmate.py
+++ tlsmate/tlsmate.py
@@ -12,6 +12,7 @@ from tlsmate.client_auth import ClientAuth
 from tlsmate.recorder import Recorder
 from tlsmate.config import Configuration
 from tlsmate.cert import Certificate
+from tlsmate.cert_chain import CertChainCache
 from tlsmate.crl_manager import CrlManager
 from tlsmate.trust_store import TrustStore
 
@@ -45,6 +46,8 @@ class TlsMate(object):
         client_auth (:obj:`tlsmate.client_auth.ClientAuth`): the object which manages
             the keys and certificate chains needed for client authentication
         client (:obj:`tlsmate.client.Client`): the client object
+        cert_chain_cache (:obj:`tlsmate.cert_chain.CertChainCache`): the certificate
+            chain validation status cache
     """
 
     instance = None
@@ -63,6 +66,7 @@ class TlsMate(object):
         self.trust_store = TrustStore(tlsmate=self)
         self.client_auth = ClientAuth(tlsmate=self)
         self.crl_manager = CrlManager(tlsmate=self)
+        self.cert_chain_cache = CertChainCache()
         key_log_file = config.get("key_log_file")
         if key_log_file:
             KeyLogger.open_file(key_log_file)
@@ -102,4 +106,3 @@ class TlsMate(object):
                 self.client_auth.deserialize_key_chain(key_chain)
 
         self.client = Client(tlsmate=self)
-        # self.work_manager = WorkManager()
