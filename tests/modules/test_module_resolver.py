fb6c59d220b8eb8ac32b89542515fdf1f1c9b8fa
Jens Guballa <jens@guballa.de>
2022-01-18 21:08:26 +0100

    Adapt test cases

--- tests/modules/test_module_resolver.py
+++ tests/modules/test_module_resolver.py
@@ -9,11 +9,6 @@ import tlsmate.structs as structs
 import tlsmate.resolver as resolver
 
 
-@pytest.fixture(autouse=True)
-def clear_cache():
-    resolver._resolved = {}
-
-
 def test_determine_l4_addr():
     l4_endpoint = resolver.determine_l4_addr("google.com", 443)
     assert l4_endpoint.host == "google.com"
@@ -32,13 +27,14 @@ def test_determine_l4_addr():
 
 
 def test_resolve_hostname():
-    resolved = resolver.resolve_hostname("example.com")
+    resolved = resolver.Resolver().resolve_hostname("example.com")
     assert resolved.ipv4_addresses == ["93.184.216.34"]
     assert resolved.ipv6_addresses == ["2606:2800:220:1:248:1893:25c8:1946"]
 
 
-def test_resolve_hostname_proxy(proxy):
-    resolved = resolver.resolve_hostname("example.com", proxy=proxy)
+def test_resolve_hostname_proxy(monkeypatch, proxy):
+    monkeypatch.setenv("http_proxy", proxy)
+    resolved = resolver.Resolver().resolve_hostname("example.com")
     assert resolved.ipv4_addresses == ["93.184.216.34"]
     assert resolved.ipv6_addresses == ["2606:2800:220:1:248:1893:25c8:1946"]
 
@@ -49,7 +45,7 @@ def test_resolve_hostname_proxy_timeout(monkeypatch):
 
     monkeypatch.setattr(dns.resolver.Resolver, "resolve", resolve)
 
-    resolved = resolver.resolve_hostname("example.com", proxy="http://localhost:8889")
+    resolved = resolver.Resolver().resolve_hostname("example.com")
     assert resolved.ipv4_addresses == []
     assert resolved.ipv6_addresses == []
 
@@ -60,7 +56,7 @@ def test_resolve_hostname_proxy_no_answer(monkeypatch):
 
     monkeypatch.setattr(dns.resolver.Resolver, "resolve", resolve)
 
-    resolved = resolver.resolve_hostname("example.com", proxy="http://localhost:8889")
+    resolved = resolver.Resolver().resolve_hostname("example.com")
     assert resolved.ipv4_addresses == []
     assert resolved.ipv6_addresses == []
 
@@ -69,7 +65,7 @@ def test_get_ip_endpoint_resolved():
     endpoint = structs.TransportEndpoint(
         host="123.123.123", port=443, host_type=tls.HostType.IPV4
     )
-    resolved = resolver.get_ip_endpoint(endpoint)
+    resolved = resolver.Resolver().get_ip_endpoint(endpoint)
     assert endpoint is resolved
 
 
@@ -77,7 +73,7 @@ def test_get_ip_endpoint_host_ipv4():
     endpoint = structs.TransportEndpoint(
         host="example.com", port=443, host_type=tls.HostType.HOST
     )
-    resolved = resolver.get_ip_endpoint(endpoint)
+    resolved = resolver.Resolver().get_ip_endpoint(endpoint)
     assert resolved.host == "93.184.216.34"
     assert resolved.port == 443
     assert resolved.host_type is tls.HostType.IPV4
@@ -87,7 +83,7 @@ def test_get_ip_endpoint_host_ipv6():
     endpoint = structs.TransportEndpoint(
         host="ipv6.test-ipv6.com", port=443, host_type=tls.HostType.HOST
     )
-    resolved = resolver.get_ip_endpoint(endpoint)
+    resolved = resolver.Resolver().get_ip_endpoint(endpoint)
     assert resolved.host == "2001:470:1:18::115"
     assert resolved.port == 443
     assert resolved.host_type is tls.HostType.IPV6
@@ -97,7 +93,7 @@ def test_get_ip_endpoint_ipv6_preference():
     endpoint = structs.TransportEndpoint(
         host="example.com", port=443, host_type=tls.HostType.HOST
     )
-    resolved = resolver.get_ip_endpoint(endpoint, ipv6_preference=True)
+    resolved = resolver.Resolver().get_ip_endpoint(endpoint, ipv6_preference=True)
     assert resolved.host == "2606:2800:220:1:248:1893:25c8:1946"
     assert resolved.port == 443
     assert resolved.host_type is tls.HostType.IPV6
